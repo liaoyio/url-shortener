8d09cbdc8a2697e2be9b7e797591422c9cd7c1b6
liaoyio <2417276459@qq.com>
2024-02-27 16:50:53 +0800

    添加 Vercel 分析服务

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { inject } from "@vercel/analytics";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+// 添加 Vercel 分析服务
+inject();
